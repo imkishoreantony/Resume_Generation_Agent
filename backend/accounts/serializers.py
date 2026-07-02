0ce2cdf0f6f15e2517fed654c04a289e0af0f73a
Academy Training <academytraining@VDARTINC.COM>
2026-07-02 00:01:14 -0400

    Implement user login API

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -16,3 +16,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data["username"],
+            password=data["password"]
+        )
+
+        if user is None:
+            raise serializers.ValidationError("Invalid username or password.")
+
+        data["user"] = user
+        return data
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,26 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(
+                {
+                    "message": "Login successful",
+                    "username": serializer.validated_data["user"].username
+                },
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
