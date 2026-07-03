8f9ed6855add226c808ff2a67eed0237b88a4b5d
imkishoreantony <kishoreantonyselvam@gmail.com>
2026-07-03 20:09:09 +0530

    Implement PDF upload and text extraction using PyMuPDF

--- backend/resumes/urls.py
+++ backend/resumes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ResumeCreateView, ResumeDetailView , ResumeUploadView
+from .views import ResumeCreateView, ResumeDetailView , ResumeUploadView , ResumeTextView
 
 urlpatterns = [
     path("", ResumeCreateView.as_view(), name="resume-list-create"),
    
     path("<int:pk>/", ResumeDetailView.as_view(), name="resume-detail"),
+    path("<int:pk>/text/", ResumeTextView.as_view(), name="resume-text"),
     
 ]
--- backend/resumes/views.py
+++ backend/resumes/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
-
+from .utils import extract_text_from_pdf
 from .models import Resume
 from .serializers import ResumeSerializer
 
@@ -35,3 +35,36 @@ class ResumeUploadView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ResumeTextView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            resume = Resume.objects.get(
+                id=pk,
+                user=request.user
+            )
+
+            if not resume.resume_file:
+                return Response({
+                    "error": "No resume file uploaded."
+                }, status=400)
+
+            text = extract_text_from_pdf(
+                resume.resume_file.path
+            )
+
+            return Response({
+                "resume_id": resume.id,
+                "title": resume.title,
+                "text": text
+            })
+
+        except Resume.DoesNotExist:
+            return Response({
+                "error": "Resume not found."
+            }, status=404)
